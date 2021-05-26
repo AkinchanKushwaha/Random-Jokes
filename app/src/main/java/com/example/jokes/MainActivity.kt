9f2770de094cadd6934af4f6de43dee24e9909c0
AkinchanKushwaha <akinchandroger@gmail.com>
2021-05-27 04:48:26 +0530

    updated the UI

--- app/src/main/java/com/example/jokes/MainActivity.kt
+++ app/src/main/java/com/example/jokes/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.jokes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.jokes.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -11,15 +12,25 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 const val BASE_URL = "https://official-joke-api.appspot.com/"
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tv_start_laughing.setOnClickListener {
+            progressBar_cyclic.visibility = View.VISIBLE
+            tv_next_joke.visibility = View.VISIBLE
+            tv_setUp.visibility = View.VISIBLE
+            tv_punchLine.visibility = View.VISIBLE
+            tv_start_laughing.visibility = View.GONE
+            ll_buttons.visibility = View.VISIBLE
+            getRandomJokes()
 
-        btn_get_new_joke.setOnClickListener {
+        }
+        tv_next_joke.setOnClickListener {
+            progressBar_cyclic.visibility = View.VISIBLE
             getRandomJokes()
         }
     }
@@ -39,11 +50,14 @@ class MainActivity : AppCompatActivity() {
                 Log.e("Jokes Data: ",responseBody.toString())
                 tv_setUp.text = responseBody.setup.toString()
                 tv_punchLine.text = responseBody.punchline.toString()
+                progressBar_cyclic.visibility = View.GONE
             }
 
             override fun onFailure(call: Call<JokesData?>, t: Throwable) {
+                progressBar_cyclic.visibility = View.GONE
                 Toast.makeText(this@MainActivity, "Failed to get data", Toast.LENGTH_SHORT).show()
             }
+
         })
     }
 
